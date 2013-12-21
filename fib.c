af996d13a2d2153ae8247c9d2db6e3a5a686ab29
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2013-12-22 00:41:51 +0100

    Update fib.c

--- fib.c
+++ fib.c
@@ -7,9 +7,9 @@ typedef unsigned long ulong;
 
 int main(int argc, char *argv[])
 {
-	/* get timer units */
-	struct timeval t1, t2;
-	gettimeofday(&t1, NULL);
+    /* get timer units */
+    struct timeval t1, t2;
+    gettimeofday(&t1, NULL);
 
   
     ulong n = atoi(argv[1]);
@@ -24,12 +24,12 @@ int main(int argc, char *argv[])
     }
 
 
-	/* get timer value */
-	gettimeofday(&t2, NULL);
-	long m1, m2, elapsed;
-	m1 = (t1.tv_sec * 1000) + (t1.tv_usec / 1000);
-	m2 = (t2.tv_sec * 1000) + (t2.tv_usec / 1000);
-	elapsed = m2 - m1;
+    /* get timer value */
+    gettimeofday(&t2, NULL);
+    long m1, m2, elapsed;
+    m1 = (t1.tv_sec * 1000) + (t1.tv_usec / 1000);
+    m2 = (t2.tv_sec * 1000) + (t2.tv_usec / 1000);
+    elapsed = m2 - m1;
 
     printf("%lu - %ld millis\n", b, elapsed);
     return 0;
